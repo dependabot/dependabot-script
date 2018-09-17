73fdc67a4e0dd23e69bad1ee27356b8a9f257e31
Grey Baker <greysteil@gmail.com>
2018-09-17 10:19:09 +0100

    Add GitHub Enterprise script

---
+++ github-enterprise-update-script.rb
@@ -0,0 +1,133 @@
+# This script is designed to loop through all dependencies in a GHE project,
+# creating PRs where necessary.
+#
+# It is intended to be used as a stop-gap until Dependabot launches an on-prem
+# solution for GitHub Enterprise (coming soon!)
+
+require "dependabot/file_fetchers"
+require "dependabot/file_parsers"
+require "dependabot/update_checkers"
+require "dependabot/file_updaters"
+require "dependabot/pull_request_creator"
+
+credentials =
+  [
+    {
+      "type" => "git_source",
+      "host" => ENV["GITHUB_ENTERPRISE_HOSTNAME"] # E.g., "ghe.mydomain.com",
+      "username" => "x-access-token",
+      "password" => ENV["GITHUB_ENTERPRISE_ACCESS_TOKEN"] # A GHE access token with API permission
+    },
+    {
+      "type" => "git_source",
+      "host" => "github.com",
+      "username" => "x-access-token",
+      "password" => ENV["GITHUB_ACCESS_TOKEN"] # A Github access token with read access to public repos
+    }
+]
+
+# Full name of the repo you want to create pull requests for.
+repo_name = ENV["PROJECT_PATH"] # myaccount/myproject
+
+# Directory where the base dependency files are.
+directory = "/"
+
+# Name of the package manager you'd like to do the update for. Options are:
+# - bundler
+# - pip (includes pipenv)
+# - npm_and_yarn
+# - maven
+# - gradle
+# - cargo
+# - hex
+# - composer
+# - nuget
+# - dep
+# - elm-package
+# - submodules
+# - docker
+# - terraform
+package_manager = ENV["PACKAGE_MANAGER"] || "bundler"
+
+source = Dependabot::Source.new(
+  provider: "github",
+  hostname: ENV["GITHUB_ENTERPRISE_HOSTNAME"],
+  api_endpoint: "https://#{ENV['GITHUB_ENTERPRISE_HOSTNAME']}/api/v3/"
+  repo: repo_name,
+  directory: directory,
+  branch: nil
+)
+
+##############################
+# Fetch the dependency files #
+##############################
+fetcher = Dependabot::FileFetchers.for_package_manager(package_manager).new(
+  source: source,
+  credentials: credentials,
+)
+
+files = fetcher.files
+commit = fetcher.commit
+
+##############################
+# Parse the dependency files #
+##############################
+parser = Dependabot::FileParsers.for_package_manager(package_manager).new(
+  dependency_files: files,
+  source: source,
+  credentials: credentials,
+)
+
+dependencies = parser.parse
+
+dependencies.each do |dep|
+  #########################################
+  # Get update details for the dependency #
+  #########################################
+  checker = Dependabot::UpdateCheckers.for_package_manager(package_manager).new(
+    dependency: dep,
+    dependency_files: files,
+    credentials: credentials,
+  )
+
+  next if checker.up_to_date?
+
+  requirements_to_unlock =
+    if !checker.requirements_unlocked_or_can_be?
+      if checker.can_update?(requirements_to_unlock: :none) then :none
+      else :update_not_possible
+      end
+    elsif checker.can_update?(requirements_to_unlock: :own) then :own
+    elsif checker.can_update?(requirements_to_unlock: :all) then :all
+    else :update_not_possible
+    end
+
+  next if requirements_to_unlock == :update_not_possible
+
+  updated_deps = checker.updated_dependencies(
+    requirements_to_unlock: requirements_to_unlock
+  )
+
+  #####################################
+  # Generate updated dependency files #
+  #####################################
+  updater = Dependabot::FileUpdaters.for_package_manager(package_manager).new(
+    dependencies: updated_deps,
+    dependency_files: files,
+    credentials: credentials,
+  )
+
+  updated_files = updater.updated_dependency_files
+
+  ########################################
+  # Create a pull request for the update #
+  ########################################
+  pr_creator = Dependabot::PullRequestCreator.new(
+    source: source,
+    base_commit: commit,
+    dependencies: updated_deps,
+    files: updated_files,
+    credentials: credentials,
+  )
+  pr_creator.create
+end
