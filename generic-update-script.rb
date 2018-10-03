52468404692868e6a3a407c9c8de1a4acc6e3770
Léo Colombaro <git@colombaro.fr>
2018-10-03 16:33:52 +0200

    Make the update script generic for GHE/GitLab

--- generic-update-script.rb
+++ generic-update-script.rb
@@ -1,8 +1,8 @@
-# This script is designed to loop through all dependencies in a GHE project,
-# creating PRs where necessary.
+# This script is designed to loop through all dependencies in a GHE or GitLab
+# project, creating PRs where necessary.
 #
-# It is intended to be used as a stop-gap until Dependabot launches an on-prem
-# solution for GitHub Enterprise (coming soon!)
+# It is intended to be used as a stop-gap until Dependabot's hosted instance
+# supports GitHub Enterprise and GitLab (coming soon!)
 
 require "dependabot/file_fetchers"
 require "dependabot/file_parsers"
@@ -10,24 +10,17 @@ require "dependabot/update_checkers"
 require "dependabot/file_updaters"
 require "dependabot/pull_request_creator"
 
-credentials =
-  [
-    {
-      "type" => "git_source",
-      "host" => ENV["GITHUB_ENTERPRISE_HOSTNAME"] # E.g., "ghe.mydomain.com",
-      "username" => "x-access-token",
-      "password" => ENV["GITHUB_ENTERPRISE_ACCESS_TOKEN"] # A GHE access token with API permission
-    },
-    {
-      "type" => "git_source",
-      "host" => "github.com",
-      "username" => "x-access-token",
-      "password" => ENV["GITHUB_ACCESS_TOKEN"] # A Github access token with read access to public repos
-    }
+credentials = [
+  {
+    "type" => "git_source",
+    "host" => "github.com",
+    "username" => "x-access-token",
+    "password" => ENV["GITHUB_ACCESS_TOKEN"] # A GitHub access token with read access to public repos
+  }
 ]
 
 # Full name of the repo you want to create pull requests for.
-repo_name = ENV["PROJECT_PATH"] # myaccount/myproject
+repo_name = ENV["PROJECT_PATH"] # namespace/project
 
 # Directory where the base dependency files are.
 directory = "/"
@@ -49,14 +42,48 @@ directory = "/"
 # - terraform
 package_manager = ENV["PACKAGE_MANAGER"] || "bundler"
 
-source = Dependabot::Source.new(
-  provider: "github",
-  hostname: ENV["GITHUB_ENTERPRISE_HOSTNAME"],
-  api_endpoint: "https://#{ENV['GITHUB_ENTERPRISE_HOSTNAME']}/api/v3/"
-  repo: repo_name,
-  directory: directory,
-  branch: nil
-)
+if ENV["GITHUB_ENTERPRISE_ACCESS_TOKEN"]
+  credentials << {
+    "type" => "git_source",
+    "host" => ENV["GITHUB_ENTERPRISE_HOSTNAME"], # E.g., "ghe.mydomain.com",
+    "username" => "x-access-token",
+    "password" => ENV["GITHUB_ENTERPRISE_ACCESS_TOKEN"] # A GHE access token with API permission
+  }
+
+  source = Dependabot::Source.new(
+    provider: "github",
+    hostname: ENV["GITHUB_ENTERPRISE_HOSTNAME"],
+    api_endpoint: "https://#{ENV['GITHUB_ENTERPRISE_HOSTNAME']}/api/v3/",
+    repo: repo_name,
+    directory: directory,
+    branch: nil,
+  )
+elsif ENV["GITLAB_ACCESS_TOKEN"]
+  gitlab_hostname = ENV["GITLAB_HOSTNAME"] || "gitlab.com"
+
+  credentials << {
+    "type" => "git_source",
+    "host" => gitlab_hostname,
+    "username" => "x-access-token",
+    "password" => ENV["GITLAB_ACCESS_TOKEN"] # A GitLab access token with API permission
+  }
+
+  source = Dependabot::Source.new(
+    provider: "gitlab",
+    hostname: gitlab_hostname,
+    api_endpoint: "https://#{gitlab_hostname}/api/v4/",
+    repo: repo_name,
+    directory: directory,
+    branch: nil,
+  )
+else
+  source = Dependabot::Source.new(
+    provider: "github",
+    repo: repo_name,
+    directory: directory,
+    branch: nil,
+  )
+end
 
 ##############################
 # Fetch the dependency files #
@@ -128,6 +155,7 @@ dependencies.each do |dep|
     dependencies: updated_deps,
     files: updated_files,
     credentials: credentials,
+    label_language: true,
   )
   pr_creator.create
 end
